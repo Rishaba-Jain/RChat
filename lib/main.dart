2cac1b2a6bb3df43fc7fa876eacc60f1f5e36cc6
Rishaba Jain <babujain86@gmail.com>
2022-01-28 23:30:02 +0530

    added ui

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'ui/message_list.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // TODO: Add Firebase Initialization
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Add MultiProvider
+    return MaterialApp(
+      title: 'RChat',
+      theme: ThemeData(primaryColor: const Color(0xFF3D814A)),
+      // TODO: Add Consumer<UserDao> here
+      home: const MessageList(),
+      // TODO: Add closing parenthesis
+    );
+  }
+}
